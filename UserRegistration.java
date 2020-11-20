7b9743a23c77c9144e24d1bdd2fcb98dde544fab
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-11-21 02:23:30 +0530

    Implemented sample email validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class UserRegistration 
 {
+	static Scanner input=new Scanner(System.in);
 	public static void checkName()
 	{
 		String pattern= "^[A-Z][a-z]{2,}$";
-		Scanner input=new Scanner(System.in);
+		
 		String name=input.next();
 		if(name.matches(pattern))
 			System.out.println("valid");
@@ -17,11 +18,25 @@ public class UserRegistration
 			checkName();
 		}
 	}
+	public static void emailValidation() 
+	{
+		String pattern="^([a-zA-Z\\.]+)([a-zA-Z]+)@([a-z]+)\\.([a-zA-Z\\.]{2,5})$";
+		System.out.println("Enter you Email  Id");
+		String email=input.next();
+		if(email.matches(pattern))
+			System.out.println("Valid Email Id");
+		else 
+		{
+			System.out.println("Invalid Email Id");
+			emailValidation();
+		}
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Enter your First Name :");
 		checkName();
 		System.out.println("Enter your Last Name :");
 		checkName();
+		emailValidation();
 	}
 }
