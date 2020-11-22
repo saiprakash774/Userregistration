38a2f84dd088faae92a808f2b869f450e5c840c8
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-11-23 02:11:08 +0530

    Refactored the code

--- ParameterizedTestUserRegistartion.java
+++ ParameterizedTestUserRegistartion.java
@@ -1,12 +1,15 @@
 package day5;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.Collection;
 import org.junit.Before;
 import org.junit.Test;
-@RunWith(Parameter.class)
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
 public class ParameterizedTestUserRegistartion 
 {
 	private String email;
@@ -31,7 +34,7 @@ public class ParameterizedTestUserRegistartion
 		return Arrays.asList(new Object[][] { { "abc@Yahoo.com", true}, { "abc-100@yahoo.com", true}, 
 		{ "abc.100@Yahoo.com", true}, { "abc111@abc.net", true}, 
 		{ "abc-100@abc.net", true}, { "abc.100@abc.com.au", true}, 
-		{ "abc@1.com", true}, { "abc@gmail.com.com", true}, { "abc+100@gmail.com", true}, { "abcYahoo.com", true},
+		{ "abc@1.com", true}, { "abc@gmail.com.com", true}, { "abc+100@gmail.com", true}, { "abcYahoo.com", false},
 		{ "abcYahoo.com", false}, { "abc@.com.my", false}, { "abc123@gmail.a", false}, { "abc123@.com",false}, 
 		{ "abc123@.com.com", false}, { ".abc@abc.com", false}, { "abc()*@gmail.com", false}, { "abc@%*.com", false}, 
 		{ "abc..2002@gmail.com", false}, { "abc.@gmail.com", false}, {"abc@gmail.com.1a", false} });
